5fb42d122974023e2f57421d950d7e45da8a8471
liazzzzz <ziangirovaaliya@mail.ru>
2021-12-17 23:43:40 +0300

    убрала коммент

--- src/main/java/com/example/website/repositories/ArticlesRepository.java
+++ src/main/java/com/example/website/repositories/ArticlesRepository.java
@@ -9,9 +9,5 @@ import java.util.Optional;
 public interface ArticlesRepository extends JpaRepository<Article, Long> {
       List<Article> findAll();
       Optional<Article> findById(Long id);
-
-//      @Query("select p from Post p left join fetch p.user order by p.createdAt desc")
-//      Post findLatestPosts(Pageable pageable);
-
-        Article findTopByOrderByCreatedAtDesc();
+      Article findTopByOrderByCreatedAtDesc();
 }
